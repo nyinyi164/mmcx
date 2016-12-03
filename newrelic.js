ac6ae25272a2e576e11282bbac06a6a0078f60bc
nyinyi164 <nnsoulkeeper@gmail.com>
2016-12-03 16:47:38 +0630

    mmcx

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '89cdf56551f9b52418fe943f28f4a660b8f349cd',
+  license_key: 89cdf56551f9b52418fe943f28f4a660b8f349cd,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
